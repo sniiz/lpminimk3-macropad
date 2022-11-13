0993f8c5f964556992ad28261be34a5763bfb13d
haley <haley.sniiz@gmail.com>
2022-11-13 10:18:44 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 # see examples in readme.md
 config = {
-    "buttons": [],
-    # the keymaps themselves
-    # example:
-    # {
-    #     "x": 0,
-    #     "y": 0,
-    #     "color": 3, # white
-    #     "on_press": lambda: print("pressed"),
-    #     "on_release": lambda: print("released")
-    # }
-    # note: you can leave on_release as None if you don't want to do anything
-    # but you will need to define on_press
-    # note #2: the colors are not rgb, intead they use proprietaty color codes (see https://github.com/FMMT666/launchpad.py#color-codes)
+    "buttons": [
+        # the keymaps themselves, e.g.
+        # {
+        #     "x": 0,
+        #     "y": 0,
+        #     "color": 3, # white
+        #     "on_press": lambda: print("pressed"),
+        #     "on_release": lambda: print("released")
+        # }
+        # note: you can leave on_release as None if you don't want to do anything
+        # but you will need to define on_press
+        # note #2: the colors are not rgb, intead they use proprietaty color codes (see https://github.com/FMMT666/launchpad.py#color-codes)
+    ],
 }
